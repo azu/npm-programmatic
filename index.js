2e89bddebf1f34f78c48ad9e3dbb9ea79b124560
azu <azuciao@gmail.com>
2017-01-02 13:01:47 +0900

    feat(npm): built-in npm module

--- index.js
+++ index.js
@@ -1,99 +1,109 @@
-const Promise = require('bluebird');
 const exec = require('child_process').exec;
-
+const npmBin = require.resolve(".bin/npm");
 module.exports = {
-	install: function(packages, opts){
-		if(packages.length == 0 || !packages || !packages.length){Promise.reject("No packages found");}
-		if(typeof packages == "string") packages = [packages];
-		if(!opts) opts = {};
-		var cmdString = "npm install " + packages.join(" ") + " "
-		+ (opts.global ? " -g":"")
-		+ (opts.save   ? " --save":"")
-		+ (opts.saveDev? " --saveDev":"")
-		+ (opts.ignoreScripts? " --ignore-scripts":"");
+  install: function(packages, opts) {
+    if (packages.length == 0 || !packages || !packages.length) {Promise.reject(new Error("No packages found"));}
+    if (typeof packages == "string") {
+      packages = [packages];
+    }
+    if (!opts) {
+      opts = {};
+    }
+    const optionArg = (opts.global ? " -g" : "")
+                                              + (opts.save ? " --save" : "")
+                                              + (opts.saveDev ? " --saveDev" : "")
+                                              + (opts.ignoreScripts ? " --ignore-scripts" : "");
+    const cmdString = `${npmBin} install ${packages.join(" ")} ${optionArg}`;
 
-		return new Promise(function(resolve, reject){
-			var cmd = exec(cmdString, {cwd: opts.cwd?opts.cwd:"/"},(error, stdout, stderr) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(true);
-				}
-			});
-			
-			if(opts.output) {
-				var consoleOutput = function(msg) {
-					console.log('npm: ' + msg);
-				};
-				
-				cmd.stdout.on('data', consoleOutput);
-				cmd.stderr.on('data', consoleOutput);
-			}
-		});
-	},
+    return new Promise(function(resolve, reject) {
+      const cmd = exec(cmdString, {cwd: opts.cwd ? opts.cwd : "/"}, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(true);
+        }
+      });
 
-	uninstall: function(packages, opts){
-		if(packages.length == 0 || !packages || !packages.length){Promise.reject(new Error("No packages found"));}
-		if(typeof packages == "string") packages = [packages];
-		if(!opts) opts = {};
-		var cmdString = "npm uninstall " + packages.join(" ") + " "
-		+ (opts.global ? " -g":"")
-		+ (opts.save   ? " --save":"")
-		+ (opts.saveDev? " --saveDev":"");
+      if (opts.output) {
+        const consoleOutput = function(msg) {
+          console.log('npm: ' + msg);
+        };
 
-		return new Promise(function(resolve, reject){
-			var cmd = exec(cmdString, {cwd: opts.cwd?opts.cwd:"/"},(error, stdout, stderr) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(true);
-				}
-			});
-			
-			if(opts.output) {
-				var consoleOutput = function(msg) {
-					console.log('npm: ' + msg);
-				};
-				
-				cmd.stdout.on('data', consoleOutput);
-				cmd.stderr.on('data', consoleOutput);
-			}
-		});
-	},
+        cmd.stdout.on('data', consoleOutput);
+        cmd.stderr.on('data', consoleOutput);
+      }
+    });
+  },
 
-	list:function(path){
-		var global = false;
-		if(!path) global = true;
-		var cmdString = "npm ls --depth=0 " + (global?"-g ":" ");
-		return new Promise(function(resolve, reject){
-			exec(cmdString, {cwd: path?path:"/"},(error, stdout, stderr) => {
-				if(stderr == ""){
-					if (stderr.indexOf("missing")== -1 && stderr.indexOf("required") == -1) {
-						return reject(error);
-					}
-				}
-				var packages = [];
-				packages = stdout.split('\n');
-				packages = packages.filter(function(item){
-					if(item.match(/^├──.+/g) != null){
-						return true
-					}
-					if(item.match(/^└──.+/g) != null){
-						return true			  		
-					}
-					return undefined;
-				});
-				packages = packages.map(function(item){
-					if(item.match(/^├──.+/g) != null){
-						return item.replace(/^├──\s/g, "");
-					}
-					if(item.match(/^└──.+/g) != null){
-						return item.replace(/^└──\s/g, "");
-					}
-				})
-				resolve(packages);
+  uninstall: function(packages, opts) {
+    if (packages.length == 0 || !packages || !packages.length) {Promise.reject(new Error(new Error("No packages found")));}
+    if (typeof packages == "string") {
+      packages = [packages];
+    }
+    if (!opts) {
+      opts = {};
+    }
+    const optionARg = (opts.global ? " -g" : "")
+                                              + (opts.save ? " --save" : "")
+                                              + (opts.saveDev ? " --saveDev" : "");
+    const cmdString = `${npmBin} uninstall ${packages.join(" ")} ${optionARg}`;
 
-			});
-		});
-	}
-}
+    return new Promise(function(resolve, reject) {
+      const cmd = exec(cmdString, {cwd: opts.cwd ? opts.cwd : "/"}, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(true);
+        }
+      });
+
+      if (opts.output) {
+        const consoleOutput = function(msg) {
+          console.log('npm: ' + msg);
+        };
+
+        cmd.stdout.on('data', consoleOutput);
+        cmd.stderr.on('data', consoleOutput);
+      }
+    });
+  },
+
+  list: function(path) {
+    let global = false;
+    if (!path) {
+      global = true;
+    }
+    const optionArg = (global ? "-g " : " ");
+    const cmdString = `${npmBin} ls --depth=0 ${optionArg}`;
+    return new Promise(function(resolve, reject) {
+      exec(cmdString, {cwd: path ? path : "/"}, (error, stdout, stderr) => {
+        if (stderr == "") {
+          if (stderr.indexOf("missing") == -1 && stderr.indexOf("required") == -1) {
+            return reject(error);
+          }
+        }
+        let packages = [];
+        packages = stdout.split('\n');
+        packages = packages.filter(function(item) {
+          if (item.match(/^├──.+/g) != null) {
+            return true
+          }
+          if (item.match(/^└──.+/g) != null) {
+            return true
+          }
+          return undefined;
+        });
+        packages = packages.map(function(item) {
+          if (item.match(/^├──.+/g) != null) {
+            return item.replace(/^├──\s/g, "");
+          }
+          if (item.match(/^└──.+/g) != null) {
+            return item.replace(/^└──\s/g, "");
+          }
+        });
+        resolve(packages);
+
+      });
+    });
+  }
+};
--- test/list/list.test.js
+++ test/list/list.test.js
@@ -2,15 +2,14 @@ var npm = require("../../index");
 
 describe("Test listing of installed packages", ()=>{
 	it("should list all installed packages by npm-programmatic", function(done){
-		this.timeout(5000);
 		npm.list('.')
 		.then(function(packages){
 			for(var i in packages){
-				if(packages[i].indexOf('bluebird')!=-1){
+				if(packages[i].indexOf('npm')!=-1){
 					return done();
 				}
 			}
-			return done(new Error("bluebird not found!"));
+			return done(new Error("npm not found!"));
 		})
 		.catch(function(err, stderr){
 			return done(err, stderr);
