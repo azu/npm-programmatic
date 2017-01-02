35c0e05e2a535810b9d410b6a886406baf758b0f
azu <azuciao@gmail.com>
2017-01-02 13:10:03 +0900

    use bluebird

--- test/install/install.test.js
+++ test/install/install.test.js
@@ -5,18 +5,18 @@ var exec = require('child_process').exec,child;
 
 describe("Test installation of packages", ()=>{
 	beforeEach(()=>{
-		child = exec('rm -rf ./node_modules/left-pad',function(err,out) {});
+		child = exec('rm -rf ./node_modules/bluebird',function(err,out) {});
 		child = exec('cp ./package.json ./test/backup/package.json',function(err,out) {});
 	});
 	afterEach(()=>{
-		child = exec('rm -rf ./node_modules/left-pad',function(err,out) {});
+		child = exec('rm -rf ./node_modules/bluebird',function(err,out) {});
 		child = exec('cp ./test/backup/package.json ./package.json' ,function(err,out) {});
 	});
 	it("should install package", function(done){
 		this.timeout(5000);
-		npm.install(["left-pad"], {cwd:'.'}).then((result)=>{
+		npm.install(["bluebird"], {cwd:'.'}).then((result)=>{
 			try{
-				var checkExists = fs.accessSync('./node_modules/left-pad');
+				var checkExists = fs.accessSync('./node_modules/bluebird');
 			} catch(err){
 				return done(err);
 			}
@@ -28,11 +28,11 @@ describe("Test installation of packages", ()=>{
 
 	it("should install package inside a node project and save it to package.json", function(done){
 		this.timeout(5000);
-		npm.install(["left-pad"], {cwd:'.', save:true}).then((result)=>{
+		npm.install(["bluebird"], {cwd:'.', save:true}).then((result)=>{
 			try{
-				var checkExists = fs.accessSync('./node_modules/left-pad');
+				var checkExists = fs.accessSync('./node_modules/bluebird');
 				var contents = require('../../package.json');
-				if(!contents.dependencies['left-pad']){
+				if(!contents.dependencies['bluebird']){
 					throw new Error();
 				}
 			} catch(err){
